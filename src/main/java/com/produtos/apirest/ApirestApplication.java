b52067e74dbd4cad9d6f671f9f792e1fc829faa5
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2019-12-05 14:48:26 -0300

    Acrescenta o Cors ao projeto

--- src/main/java/com/produtos/apirest/ApirestApplication.java
+++ src/main/java/com/produtos/apirest/ApirestApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(scanBasePackages="com.produtos")
 @EnableJpaRepositories(basePackages="com.produtos")
@@ -14,6 +17,15 @@ public class ApirestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApirestApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
 	}
 
 }
