cc80ce9dc9800dbb8f24ce3a3b31692247496203
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2019-12-05 14:49:40 -0300

    Acrescenta os métodos getAll, getById e Post ao Resource de Produtos

--- src/main/java/com/produtos/apirest/repository/ProdutoRepository.java
+++ src/main/java/com/produtos/apirest/repository/ProdutoRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.produtos.apirest.models.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+	
+	Produto findById(long id); //tipo nome_metodo(tipoAtributo atributo)
 
 }
